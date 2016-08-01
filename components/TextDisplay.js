ccc1b5fdc725dd987b1be64588aa38834a3c96f1
Eddy Lu <eddlu@microsoft.com>
2016-08-01 16:54:57 -0700

    Complete Part 5

---
+++ components/TextDisplay.js
@@ -0,0 +1,10 @@
+import React, {Component } from 'react'
+
+class TextDisplay extends Component {
+
+    render() {
+        return <div>I'm going to display from my parent: {this.props.text}</div>
+    }
+}
+
+export default TextDisplay
